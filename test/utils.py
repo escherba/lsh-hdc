579a6ccfa42e19dfa78b55bc0356585a2fa9862a
Eugene Scherba <escherba@livefyre.com>
2014-01-05 22:44:54 -0800

    add more docstrings

--- test/utils.py
+++ test/utils.py
@@ -11,6 +11,12 @@ from itertools import imap
 
 
 def read_json_file(file_path):
+    """Open a JSON file and read contents
+    :param file_path: path to file
+    :type file_path: str
+    :returns: iterator of JSON objects
+    :rtype: collections.iterable
+    """
     with open(file_path, 'r') as json_file:
         for line in json_file:
             yield json.loads(line)
@@ -67,24 +73,33 @@ def last(it):
 def gapply(n, func, *args, **kwargs):
     """Apply a generating function n times to the argument list
 
+    :param n: number of times to apply a function
+    :type n: integer
+    :param func: a function to apply
+    :type func: instancemethod
     :rtype: collections.iterable
     """
     for _ in xrange(n):
         yield func(*args, **kwargs)
 
 
-def lapply(*args, **kwargs):
-    """Same as gapply except treturn a list
+def lapply(n, func, *args, **kwargs):
+    """Same as gapply, except returns a list
 
+    :param n: number of times to apply a function
+    :type n: integer
+    :param func: a function to apply
+    :type func: instancemethod
     :rtype: list
     """
-    return list(gapply(*args, **kwargs))
+    return list(gapply(n, func, *args, **kwargs))
 
 
 def randset():
     """Return a random set.  These values of n and k have wide-ranging
     similarities between pairs.
 
+    :returns: a list of integers
     :rtype: tuple
     """
     n = random.choice(range(5, 20))
@@ -93,7 +108,16 @@ def randset():
 
 
 def sigsim(x, y, dim):
-    """Return the similarity of the two signatures"""
+    """Return the similarity of the two signatures
+    :param x: signature 1
+    :type x: object
+    :param y: signature 2
+    :type y: object
+    :param dim: number of dimensions
+    :type dim: int
+    :returns: similarity between two signatures
+    :rtype: float
+    """
     return float(sum(imap(operator.eq, x, y))) / float(dim)
 
 
@@ -101,6 +125,10 @@ def sort_by_length(els, reverse=True):
     """Given a list of els, sort its elements by len()
     in descending order. Returns a generator
 
+    :param els: input list
+    :type els: list
+    :param reverse: Whether to reverse a list
+    :type reverse: bool
     :rtype: collections.iterable
     """
     return imap(first,
