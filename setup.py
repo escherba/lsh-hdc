c364209290be6ab6a1e490532dec538d7816797a
Eugene Scherba <escherba@livefyre.com>
2014-07-27 17:02:09 -0700

    drop suffix

--- setup.py
+++ setup.py
@@ -11,17 +11,23 @@ dev_requirements = resource_string(
 # regex for finding URLs in strings
 GRUBER_URLINTEXT_PAT = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
 contains_url = partial(re.findall, GRUBER_URLINTEXT_PAT)
-EGG_PATTERN = re.compile(r'#egg=(\w+((-|>=|==)?[\d\w.]+)?)$')
+EGG_PATTERN = re.compile(ur'(.+)#egg=(\w+((-|>=|==)?[\d\w.]+)?)$')
 find_eggs = partial(re.search, EGG_PATTERN)
 
-dependency_links = filter(contains_url, requirements)
-install_requires = \
-    filter(lambda r: not contains_url(r), requirements) + \
-    map(lambda x: find_eggs(x).groups()[0], requirements)
+pkg_names = []
+dep_links = []
+for req in requirements:
+    if contains_url(req):
+        url, egg = find_eggs(req).groups()[0:1]
+        pkg_names.append(egg)
+        dep_links.append(url)
+    else:
+        pkg_names.append(req)
+
 tests_require = filter(lambda r: not contains_url(r), dev_requirements)
 
-print "install_requires: " + str(install_requires)
-print "dependency_links: " + str(dependency_links)
+print "install_requires: " + str(pkg_names)
+print "dependency_links: " + str(dep_links)
 
 setup(
     name="lsh-hdc",
@@ -32,8 +38,8 @@ setup(
     url='https://github.com/escherba/lsh-hdc',
     packages=find_packages(exclude=['tests', 'scripts']),
     long_description="LSH algo that uses MinHash signatures",
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=pkg_names,
+    dependency_links=dep_links,
     tests_require=tests_require,
     test_suite='nose.collector',
     classifiers=[
