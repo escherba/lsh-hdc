6ff935b8b2c7beeba10d408df5e44b812c31916e
Eugene Scherba <escherba@livefyre.com>
2014-07-30 22:48:08 -0700

    add cluster uniquing step

--- tests/mr_cluster_mac_log.py
+++ tests/mr_cluster_mac_log.py
@@ -1,41 +1,87 @@
 #!/usr/bin/env python2
 
+import os
 import yaml
 from mrjob import protocol as mrjob_protocol
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 from operator import itemgetter
+from collections import Counter
 from lsh_hdc.cluster import HDClustering
 from content_rules import ContentFilter
-from pkg_resources import resource_string
+
+import sys
+
+fn = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/mac.yaml'))
+sys.stderr.write("--> " + fn + "\n")
+with open(fn, 'r') as fh:
+    mac_cfg = yaml.load(fh)
 
 
 class MRCluster(MRJob):
 
     INPUT_PROTOCOL = mrjob_protocol.JSONValueProtocol
-    OUTPUT_PROTOCOL = mrjob_protocol.JSONValueProtocol
+    INTERNAL_PROTOCOL = mrjob_protocol.JSONProtocol
+    OUTPUT_PROTOCOL = mrjob_protocol.JSONProtocol
 
     def __init__(self, hdc=None, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
         self.hdc = hdc
 
-    def mapper(self, _, data):
+    def lsh_mapper(self, _, data):
         obj = data['object']
         for pair in hdc.mapper(obj):
             yield pair
 
-    def combiner(self, key, vals):
+    def lsh_combiner(self, key, vals):
         ckey, cvals = hdc.reducer(key, vals)
         for cval in cvals:
             yield ckey, cval
 
-    def reducer(self, key, vals):
+    def lsh_reducer(self, key, vals):
         rkey, rvals = hdc.reducer(key, vals)
         if len(rvals) > 1:
             yield rkey, rvals
 
+    def ab_mapper(self, _, data):
+        sorted_data = sorted(data)
+        key = sorted_data[0][0]
+        yield key, sorted_data
+
+    def cluster_reducer(self, key, vals):
+        uniq = set()
+        for val in vals:
+            uniq.add(tuple(tuple(v) for v in val))
+        for val in uniq:
+            yield key, val
+
+    def sketch_mapper(self, _, data):
+
+        # find most commonly occuring sketch and pick one associated
+        # with alphabetically lowest id
+
+        c = Counter()
+        cluster = list(data)
+        for label, sketch in cluster:
+            c[sketch] += 1
+        label2sketch = dict(cluster)
+        label_c = [(label, c[sketch]) for label, sketch in cluster]
+        sorted_data = sorted(label_c, key=lambda x: (-x[1], x[0]))
+        output = [(label, label2sketch[label]) for label, cnt in sorted_data]
+        key = output[0][1]
+        yield key, output
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.lsh_mapper,
+                   combiner=self.lsh_combiner,
+                   reducer=self.lsh_reducer),
+            MRStep(mapper=self.sketch_mapper,
+                   reducer=self.cluster_reducer)
+        ]
+
 
 if __name__ == '__main__':
-    mac_cfg = yaml.load(resource_string(__name__, 'data/mac.yaml'))
     hdc = HDClustering(cfg=mac_cfg['model'],
                        content_filter=ContentFilter(),
                        get_body=itemgetter('content'),
