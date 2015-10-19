b0b8808e9c2c0b67cd6305c57a6cafedfa7c1589
Eugene Scherba <escherba@gmail.com>
2015-10-19 13:31:03 -0700

    reset conf to boring defaults

--- experiment/conf.mk
+++ experiment/conf.mk
@@ -1,4 +1,4 @@
-PYSCRIPT := lsh_hdc.study joint --pos_ratio 0.2 --p_err 0.1 --seq_len_min 12 --sim_size 60000
+PYSCRIPT := lsh_hdc.study joint --pos_ratio 0.2 --p_err 0.0 --seq_len_min 3 --sim_size 60000
 CLUSTER_SIZES := $(shell for i in `seq 1 6`; do python -c "print 2 ** $$i"; done)
 SEEDS := $(shell seq 0 5)
 HASHES := metrohash md5 builtin cityhash
