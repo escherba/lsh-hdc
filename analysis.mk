657cef4760dc548f3746ab34e9b115cb5be3777c
Eugene Scherba <escherba@gmail.com>
2015-10-19 12:18:03 -0700

    can rebuild derivative result files at will in experiment directory

--- analysis.mk
+++ analysis.mk
@@ -1,21 +1,38 @@
+# disable built-in rules
+.SUFFIXES:
+
 include experiment/include.mk
 include experiment/misc.mk
 
+# If OUTPUT_DIR is not set externally, define it. This
+# lets us build make targets while passing a parameter:
+#
+#   OUTPUT_DIR=/my/custom/dir make analysis
+#
+ifeq ($(OUTPUT_DIR),)
 OUTPUT_DIR := experiment/out-$(shell date +%Y%m%d%H%M%S)
+endif
 
 FILENAMES := $(shell for csz in $(CLUSTER_SIZES); do for h in $(HASHES); do for s in $(SEEDS); do printf "$(OUTPUT_DIR)/%03d-%s-%d.json " $$csz $$h $$s; done; done; done)
 
 
+# Intermediate files will be deleted once done
 .INTERMEDIATE: $(FILENAMES)
 
-$(OUTPUT_DIR)/%.json: experiment/misc.mk
+$(OUTPUT_DIR)/%.mk: experiment/%.mk
+	@mkdir -p $(dir $@)
+	if [ ! -f "$@" ]; then cp -p $< $@; fi
+
+$(OUTPUT_DIR)/%.json: $(OUTPUT_DIR)/misc.mk
 	@mkdir -p $(dir $@)
 	$(PYTHON) -m lsh_hdc.study joint $(MISC_ARGS) --output $@ \
 		--cluster_size $(word 1,$(subst -, ,$*)) \
 		--hashfun $(word 2,$(subst -, ,$*)) \
 		--seed $(word 3,$(subst -, ,$*))
 
-.SECONDARY: $(OUTPUT_DIR)/summary.ndjson $(OUTPUT_DIR)/summary.csv
+
+# Secondary files will be kept
+.SECONDARY: $(OUTPUT_DIR)/include.mk $(OUTPUT_DIR)/misc.mk $(OUTPUT_DIR)/summary.ndjson $(OUTPUT_DIR)/summary.csv
 
 $(OUTPUT_DIR)/summary.ndjson: $(FILENAMES)
 	@mkdir -p $(dir $@)
@@ -25,6 +42,6 @@ $(OUTPUT_DIR)/summary.csv: $(OUTPUT_DIR)/summary.ndjson
 	@mkdir -p $(dir $@)
 	$(PYTHON) -m lsh_hdc.study summary --input $< --output $(dir $@)
 
+.PHONY: analysis
 analysis: $(OUTPUT_DIR)/summary.csv
-	cp experiment/*.mk $(OUTPUT_DIR)/
 	@echo "all done"
