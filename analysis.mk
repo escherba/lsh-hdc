69364819b94d999d47478330eb5198ea81f1d97d
Eugene Scherba <escherba@gmail.com>
2015-10-19 04:26:57 -0700

    simplify makefile

--- analysis.mk
+++ analysis.mk
@@ -3,28 +3,29 @@ include experiment/misc.mk
 
 OUTPUT_DIR := experiment/out
 
-FILENAMES := $(shell for csz in $(CLUSTER_SIZES); do for h in $(HASHES); do for s in $(SEEDS); do printf "experiment/%03d-%s-%d.json " $$csz $$h $$s; done; done; done)
+FILENAMES := $(shell for csz in $(CLUSTER_SIZES); do for h in $(HASHES); do for s in $(SEEDS); do printf "$(OUTPUT_DIR)/%03d-%s-%d.json " $$csz $$h $$s; done; done; done)
 
 
 $(OUTPUT_DIR)/%.json: experiment/misc.mk
 	@mkdir -p $(dir $@)
+	echo $(word 1,$(subst -, ,$*))
 	$(PYTHON) -m lsh_hdc.study joint $(MISC_ARGS) --output $@ \
 		--cluster_size $(word 1,$(subst -, ,$*)) \
 		--hashfun $(word 2,$(subst -, ,$*)) \
 		--seed $(word 3,$(subst -, ,$*))
 
-$OUTPUT_DIR)/summary.json: $(FILENAMES) | experiment/include.mk
-	@mkdir -p $(dir $@)
-	cat $^ > $@
 
-.SECONDARY: $(OUTPUT_DIR)/summary.csv $(OUTPUT_DIR)/summary.json
+.SECONDARY: $(OUTPUT_DIR)/summary.csv
 
-$(OUTPUT_DIR)/summary.csv: $(OUTPUT_DIR)/summary.json
+$(OUTPUT_DIR)/summary.csv: $(FILENAMES)
 	@mkdir -p $(dir $@)
-	$(PYTHON) -m lsh_hdc.study summary --input $< --output $(dir $@)
+	$(PYTHON) -m lsh_hdc.study summary --input $^ --output $(dir $@)
 
 analysis: $(OUTPUT_DIR)/summary.csv
 	@echo "all done"
 
 analysis_clean:
 	rm -rf $(OUTPUT_DIR)
+
+test_test:
+	@echo $(FILENAMES)
