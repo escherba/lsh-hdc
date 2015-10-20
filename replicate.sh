1f52cbb559f933f79aae18391ac78a5b702ba057
Eugene Scherba <escherba@gmail.com>
2015-10-20 03:06:21 -0700

    naming..

--- replicate.sh
+++ replicate.sh
@@ -6,8 +6,8 @@ EXP_DIRS=experiment/exp-*
 
 make -r build_ext
 find $EXP_DIRS -type f -name $TARGET -print0 | \
-    while IFS= read -r -d '' config_file; do
-        experiment=`dirname "$config_file"`
+    while IFS= read -r -d '' target_path; do
+        experiment=`dirname "$target_path"`
         echo "replicating '$experiment'"
         touch "$experiment/$TARGET"
         EXPERIMENT="$experiment" make -r -j$NUM_PROCS analysis
