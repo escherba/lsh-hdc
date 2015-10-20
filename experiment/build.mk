ae0ea8ef0d9d6cc90a04a858eb42ca5d2ed35bfb
Eugene Scherba <escherba@gmail.com>
2015-10-19 23:22:01 -0700

    allow easy replicagion of entire experiment suite

--- experiment/build.mk
+++ experiment/build.mk
@@ -8,7 +8,7 @@ ifeq ($(OUTPUT_DIR),)
 # OUTPUT_DIR has not been defined, so generate a name for
 # one and include default experiment config file
 OUTPUT_DIR := $(CURRENT_DIR)/out-$(shell date +%Y%m%d%H%M%S)
-include $(CURRENT_DIR)/conf.mk
+include $(CURRENT_DIR)/config.mk
 else
 # OUTPUT_DIR has been defined, include the make config from
 # that directory only. It's likely that the following
@@ -16,7 +16,7 @@ else
 #
 #    OUTPUT_DIR=/my/custom/dir make analysis
 #
-include $(OUTPUT_DIR)/conf.mk
+include $(OUTPUT_DIR)/config.mk
 endif
 
 
@@ -26,31 +26,41 @@ FILENAMES := $(shell for csz in $(CLUSTER_SIZES); do for h in $(HASHES); do for 
 # Intermediate files will be deleted once make is done
 .INTERMEDIATE: $(FILENAMES)
 
-$(OUTPUT_DIR)/%.json: $(OUTPUT_DIR)/conf.mk
+$(OUTPUT_DIR)/%.json: $(OUTPUT_DIR)/config.mk
 	@mkdir -p $(dir $@)
-	$(PYTHON) -m $(PYSCRIPT) --output $@ \
+	@echo "Building $*"
+	@$(PYTHON) -m lsh_hdc.study joint $(BUILD_ARGS) --output $@ \
 		--cluster_size $(word 1,$(subst -, ,$*)) \
 		--hashfun $(word 2,$(subst -, ,$*)) \
 		--seed $(word 3,$(subst -, ,$*))
 
 
 # Secondary files will be kept
-.SECONDARY: $(OUTPUT_DIR)/conf.mk $(OUTPUT_DIR)/summary.ndjson $(OUTPUT_DIR)/summary.csv
+.SECONDARY: $(OUTPUT_DIR)/config.mk $(OUTPUT_DIR)/summary.ndjson $(OUTPUT_DIR)/summary.csv
 
 $(OUTPUT_DIR)/%.mk: $(CURRENT_DIR)/%.mk
 	@mkdir -p $(dir $@)
-	if [ ! -f "$@" ]; then cp -p $< $@; fi
+	@if [[ ! -f "$@" ]]; then cp -p $< $@; fi
 
 $(OUTPUT_DIR)/summary.ndjson: $(FILENAMES)
 	@mkdir -p $(dir $@)
-	cat $^ > $@
+	@cat $^ > $@
 
 $(OUTPUT_DIR)/summary.csv: $(OUTPUT_DIR)/summary.ndjson
 	@mkdir -p $(dir $@)
-	$(PYTHON) -m lsh_hdc.study summary --input $< --output $(dir $@)
+	@echo "Creating summary file at $@"
+	@$(PYTHON) -m lsh_hdc.study summary --input $< --output $(dir $@)
+	@echo "compressing $(OUTPUT_DIR)..."
+	@tar czf ` \
+		if [[ -e $(OUTPUT_DIR).tgz ]]; \
+			then i=0; \
+			while [[ -e $(OUTPUT_DIR)-$$i.tgz ]]; \
+				do let i++; \
+			done; \
+			echo $(OUTPUT_DIR)-$$i.tgz; \
+		else echo $(OUTPUT_DIR).tgz; \
+		fi` $(OUTPUT_DIR)
 
 .PHONY: analysis
 analysis: $(OUTPUT_DIR)/summary.csv
-	@echo "compressing $(OUTPUT_DIR)..."
-	tar czf $(OUTPUT_DIR).tgz $(OUTPUT_DIR)
 	@echo "all done"
--- run_domino.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-make -r build_ext
-make -r -j10 analysis
