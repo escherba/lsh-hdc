a7080c230f658faaa337a66f07f709d14291095f
Eugene Scherba <escherba@gmail.com>
2015-10-20 02:31:33 -0700

    more semantic naming

--- experiment/build.mk
+++ experiment/build.mk
@@ -4,30 +4,30 @@
 MKFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 CURRENT_DIR := $(notdir $(patsubst %/,%,$(dir $(MKFILE_PATH))))
 
-ifeq ($(OUTPUT_DIR),)
-# OUTPUT_DIR has not been defined, so generate a name for
+ifeq ($(EXPERIMENT),)
+# EXPERIMENT has not been defined, so generate a name for
 # one and include default experiment config file
-OUTPUT_DIR := $(CURRENT_DIR)/out-$(shell date +%Y%m%d%H%M%S)
+EXPERIMENT := $(CURRENT_DIR)/exp-$(shell date +%Y%m%d%H%M%S)
 include $(CURRENT_DIR)/config.mk
 else
-# OUTPUT_DIR has been defined, include the make config from
+# EXPERIMENT has been defined, include the make config from
 # that directory only. It's likely that the following
 # has been invoked:
 #
-#    OUTPUT_DIR=/my/custom/dir make analysis
+#    EXPERIMENT=/my/custom/dir make analysis
 #
-include $(OUTPUT_DIR)/config.mk
+include $(EXPERIMENT)/config.mk
 endif
 
 
-FILENAMES := $(shell for csz in $(CLUSTER_SIZES); do for h in $(HASHES); do for s in $(SEEDS); do printf "$(OUTPUT_DIR)/%03d-%s-%d.json " $$csz $$h $$s; done; done; done)
+FILENAMES := $(shell for csz in $(CLUSTER_SIZES); do for h in $(HASHES); do for s in $(SEEDS); do printf "$(EXPERIMENT)/%03d-%s-%d.json " $$csz $$h $$s; done; done; done)
 
 
 # We delete $(FILENAMES) manually for cleaner console output,
 # so let Make think they are secondary targets
 .SECONDARY: $(FILENAMES)
 
-$(OUTPUT_DIR)/%.json: $(OUTPUT_DIR)/config.mk
+$(EXPERIMENT)/%.json: $(EXPERIMENT)/config.mk
 	@mkdir -p $(dir $@)
 	@$(PYTHON) -m lsh_hdc.study joint $(BUILD_ARGS) --output $@ \
 		--cluster_size $(word 1,$(subst -, ,$*)) \
@@ -36,36 +36,36 @@ $(OUTPUT_DIR)/%.json: $(OUTPUT_DIR)/config.mk
 
 
 # Secondary files will be kept
-.SECONDARY: $(OUTPUT_DIR)/config.mk $(OUTPUT_DIR)/summary.ndjson $(OUTPUT_DIR)/summary.csv
+.SECONDARY: $(EXPERIMENT)/config.mk $(EXPERIMENT)/summary.ndjson $(EXPERIMENT)/summary.csv
 
-$(OUTPUT_DIR)/%.mk: $(CURRENT_DIR)/%.mk
+$(EXPERIMENT)/%.mk: $(CURRENT_DIR)/%.mk
 	@mkdir -p $(dir $@)
 	@echo "copying $< => $@"
 	@if [ ! -e "$@" ]; then cp -p $< $@; fi
 
-$(OUTPUT_DIR)/summary.ndjson: $(FILENAMES)
+$(EXPERIMENT)/summary.ndjson: $(FILENAMES)
 	@mkdir -p $(dir $@)
 	@cat $^ > $@
 	@rm -f $^
 
-$(OUTPUT_DIR)/summary.csv: $(OUTPUT_DIR)/summary.ndjson
+$(EXPERIMENT)/summary.csv: $(EXPERIMENT)/summary.ndjson
 	@mkdir -p $(dir $@)
-	@echo "writing 'summary.csv' in $(OUTPUT_DIR)"
+	@echo "writing 'summary.csv' in $(EXPERIMENT)"
 	@$(PYTHON) -m lsh_hdc.study summary \
 		--title "$(BUILD_ARGS)" \
 		--input $< \
 		--output $(dir $@)
-	@echo "archiving $(OUTPUT_DIR)"
+	@echo "archiving $(EXPERIMENT)"
 	@tar czf ` \
-		if [ -e $(OUTPUT_DIR).tgz ]; \
+		if [ -e $(EXPERIMENT).tgz ]; \
 			then i=0; \
-			while [ -e $(OUTPUT_DIR)-$$i.tgz ]; \
+			while [ -e $(EXPERIMENT)-$$i.tgz ]; \
 				do let i++; \
 			done; \
-			echo $(OUTPUT_DIR)-$$i.tgz; \
-		else echo $(OUTPUT_DIR).tgz; \
-		fi` $(OUTPUT_DIR)
+			echo $(EXPERIMENT)-$$i.tgz; \
+		else echo $(EXPERIMENT).tgz; \
+		fi` $(EXPERIMENT)
 
 .PHONY: analysis
-analysis: $(OUTPUT_DIR)/summary.csv
-	@echo "done building $(OUTPUT_DIR)"
+analysis: $(EXPERIMENT)/summary.csv
+	@echo "done with $(EXPERIMENT)"
--- replicate.sh
+++ replicate.sh
@@ -3,10 +3,10 @@
 NUM_PROCS=24
 
 make -r build_ext
-ls -d experiment/out-*/config.mk | \
-    while read f; do
-        dname=`dirname $f`
-        echo "replicating $dname"
-        touch $f
-        OUTPUT_DIR=$dname make -r -j$NUM_PROCS analysis
+find experiment/exp-* -type f -name config.mk -print0 | \
+    while IFS= read -r -d '' config_file; do
+        experiment=`dirname "$config_file"`
+        echo "replicating '$experiment'"
+        touch "$config_file"
+        EXPERIMENT="$experiment" make -r -j$NUM_PROCS analysis
     done
