4293c96c49c2d1793d8f746fd39eab9a3ee93d8d
Eugene Scherba <escherba@gmail.com>
2015-10-21 19:05:14 -0700

    change wording

--- experiment.sh
+++ experiment.sh
@@ -4,5 +4,5 @@ NUM_PROCS=32
 
 make -r extras
 make -r build_ext
-echo "Experiment 1 out of 1: building using $NUM_PROCS processes"
+echo "Experiment 1 out of 1: running with $NUM_PROCS processes"
 time make -r -j$NUM_PROCS experiment
