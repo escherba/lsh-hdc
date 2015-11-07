39c72d00c3cc41129c2d75c7639cdd37ba0bb244
Eugene Scherba <escherba@gmail.com>
2015-11-06 23:50:51 -0800

    misc

--- lsh_hdc/entropy.pyx
+++ lsh_hdc/entropy.pyx
@@ -13,13 +13,7 @@ np.import_array()
 
 
 cdef extern from "gamma.h":
-    cdef double sklearn_lgamma(double x)
-
-
-cdef double lgamma(double x):
-    if x <= 0:
-        raise ValueError("x must be strictly positive, got %f" % x)
-    return sklearn_lgamma(x)
+    cdef np.float64_t sklearn_lgamma(np.float64_t x)
 
 
 cpdef ndarray_from_iter(iterable, dtype=None, contiguous=False):
